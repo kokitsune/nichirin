f3abfe8ec89170b61f2512fcac4080017c737dff
kokitsune <s7070070@kmitl.ac.th>
2014-11-20 11:24:49 +0700

    +CipherDisk

--- uEnkrypt.py
+++ uEnkrypt.py
@@ -9,16 +9,19 @@ class XOR(object):
     For operation concerning XOR method.
     Attribute : A sequence of character.
     '''
-    def __init__(self, original):
+    def __init__(self, original='Default', key='00000000'):
         self.original = original
+        self.key = '0'*(8-len(key))+key.replace('b', '')
     def __str__(self):
         return self.original
-    def get_key(self, stat, key):
+    def get_key(self, stat='bin', key='00000000'):#Maximum MUST be 01111111
         if stat == 'character':
-            key = bin(ord(key))
+            self.key = bin(ord(key))
         elif stat == 'num':
-            key = bin(key)
-        return '%8s'.replace(' ', '0') % key.replace('b', '')
+            self.key = bin(key)
+        self.key = key
+        self.key = '0'*(8-len(self.key))+self.key.replace('b', '')
+        return self.key
     def encryption(self):
         '''
         It's both the encryption and decryption actually.
@@ -40,5 +43,69 @@ class XOR(object):
         A layout for other methods.
         '''
         pass
+
+class CipherDisk(object):
+    '''
+    For operation concerning Cipher Disk method.
+    Attribute: A sequence of character, number of turns
+    '''
+    def __init__(self, original='Default', turn=1, disk=None):
+        self.original = original
+        self.turn = turn
+        if disk == None:
+            self.disk = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    def disk_tuning(self, position, char):
+        if position >= len(self.disk):
+            self.disk.append(char.upper())
+        elif char == 'delete':
+            self.disk.pop(position)
+        else:
+            self.disk[position] = char.upper()
+        return self.disk
+    def encryption(self):
+        self.encrypted = ''
+        for longgong in self.original:
+            if longgong in self.disk:
+                self.encrypted += self.disk[(self.disk.index(longgong)+self.turn)%len(self.disk)]
+            elif longgong.upper() in self.disk:
+                self.encrypted += self.disk[(self.disk.index(longgong.upper())+self.turn)%len(self.disk)].lower()
+            else:
+                self.encrypted += longgong
+        return self.encrypted
+    def decryption(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
